5e226a4b2928f367f9b119df11b74326cb9a8e12
oops-yu <473211890@qq.com>
2023-08-18 19:41:02 +0800

    test submodule

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
-
+#include<mymath.hpp>
 int main(){
+    mymath my;
     std::cout<<"hello world"<<std::endl;
+    std::cout<<my.add(1,2)<<std::endl;
     return 0;
 }
---
+++ src/mymath/mymath.cpp
@@ -0,0 +1,7 @@
+#include "mymath.hpp"
+int mymath::add(int n1,int n2){
+    return n1+n2;
+}
+int mymath::sub(int n1,int n2){
+    return n1-n2;
+}
